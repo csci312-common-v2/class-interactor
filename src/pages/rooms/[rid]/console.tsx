354145a57161cff2495146736aad073c79d213fe
Michael Linderman <mlinderman@middlebury.edu>
2024-01-16 14:25:04 -0500

    Don't show console if the user is not authenticated and authorized

--- src/pages/rooms/[rid]/console.tsx
+++ src/pages/rooms/[rid]/console.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
 import { SocketProvider } from "@/components/contexts/socket/useSocketContext";
 import Console from "@/components/interfaces/console/console";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import Room from "@/models/Room";
 
 type RoomProp =
@@ -21,6 +22,7 @@ const Page = ({
     return (
       <SocketProvider roomId={room.id} admin>
         <Console room={room} />
+        <button onClick={() => signOut()}>Sign out</button>
       </SocketProvider>
     );
   } else {
@@ -30,19 +32,54 @@ const Page = ({
 };
 
 export const getServerSideProps = (async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (!session) {
+    // Mimic the behavior of useSession required: true
+    // https://github.com/nextauthjs/next-auth/blob/9e802b00fdfbec7c3008ac2853ccf3a49e26f5c0/packages/next-auth/src/react.tsx#L140C7-L143C11
+    const url = `/api/auth/signin?${new URLSearchParams({
+      error: "SessionRequired",
+      callbackUrl: context.resolvedUrl,
+    })}`;
+    return {
+      redirect: {
+        destination: url,
+        permanent: false,
+      },
+    };
+  }
+
   const { rid } = context.query;
-  let room = await Room.query().findOne({ visibleId: rid });
-  if (room) {
+  const [room] = await Room.query()
+    .where({ visibleId: rid })
+    .withGraphFetched("users(filterIdAndRole)")
+    .modifiers({
+      filterIdAndRole(builder) {
+        builder.where({ id: session.user.id, role: "administrator" });
+      },
+    });
+
+  if (room && room.users.length > 0) {
+    // This user is an admin of this room
     return {
       props: {
         room: {
           id: room.visibleId,
           name: room.name,
         },
+        session,
       },
     };
   } else {
-    return { props: { room } };
+    // Invalid room or user is not an admin
+    const url = `/?${new URLSearchParams({
+      error: "InvalidRoom",
+    })}`;
+    return {
+      redirect: {
+        destination: url,
+        permanent: false,
+      },
+    };
   }
 }) satisfies GetServerSideProps<{ room: RoomProp }>;
 
