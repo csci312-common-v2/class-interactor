7495e302d4bd8a2074ad0465a8bf3b766c5dbe40
Katie Macalintal <kmacalintal@middlebury.edu>
2024-02-01 11:28:57 -0500

    Created a basic about page on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Container, Typography } from "@mui/material";
 import Head from "next/head";
 
 export default function Home() {
@@ -8,7 +9,36 @@ export default function Home() {
         <meta name="description" content="Class interaction application" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main />
+      <main>
+        <Container maxWidth="lg">
+          <Typography variant="h4" gutterBottom sx={{ mt: 1 }}>
+            Class Interactor
+          </Typography>
+          <Typography variant="body1" sx={{ fontSize: "h6.fontSize" }}>
+            Class Interactor is an application designed to facilitate real time
+            digital interaction in a classroom setting.
+          </Typography>
+          <Typography variant="h6" sx={{ mt: 1 }}>
+            Features
+          </Typography>
+          <Typography component="div" variant="body1">
+            <ul>
+              <li>Question Polling</li>
+              <li>Question Board</li>
+              <li>Reminders Board</li>
+              <li>Reactions</li>
+              <li>... and more!</li>
+            </ul>
+          </Typography>
+          <Typography variant="h6">Contributors</Typography>
+          <Typography component="div" variant="body1">
+            <ul>
+              <li>Professor Michael Linderman</li>
+              <li>Katie Macalintal, Class of 2024</li>
+            </ul>
+          </Typography>
+        </Container>
+      </main>
     </>
   );
 }
